481fb5ca22dd720f2f0dba2ad42c3f66183fa784
Ilya Shevchenko <id.shev@yandex.ru>
2019-11-23 16:19:54 +0300

    New files and map

--- src/main.js
+++ src/main.js
@@ -1,7 +1,15 @@
 import Vue from 'vue';
+import YmapPlugin from 'vue-yandex-maps'
 import App from './App.vue';
 import router from './router';
 import store from './store';
+
+Vue.use(YmapPlugin, {
+  apiKey: '1a7faacc-6e90-4cca-a2d2-5330e29dba7b',
+  lang: 'ru_RU',
+  coordorder: 'latlong',
+  version: '2.1',
+})
 
 Vue.config.productionTip = false;
 
